98ce46eba544b9699112ea00f46b1fb53b935064
Toan <tpkhuong@gmail.com>
2020-12-16 17:11:36 -0500

    added webpack dev server and live reload. works

--- src/ourFunctions.js
+++ src/ourFunctions.js
@@ -1,6 +1,8 @@
 import { ourChars as firstObj, ourFavorites as secondObj } from "./chars";
 
 export function sayHi() {
+    console.log("this is from inside of sayHi function");
+    console.log("Hello from ourFunctions.js");
     console.log(`this char says hi: firstName:${firstObj.firstChar.firstName}, lastName: ${firstObj.firstChar.lastName}. They live in state: ${secondObj.firstFavoriteThings.state}`);
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 var path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 module.exports = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "./dist")
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        //once we added HtmlWebpackPlugin and pass in the obj {} with the template property pointing to our index.html file in dist live-reload works. WOOT!
+        template: "./dist/index.html"
+    })],
+    devtool: "source-map",
+    devServer: {
+        contentBase: "./dist"
     }
 }
