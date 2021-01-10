32a8a5a3e0eee10971966d9f3461e1b717b701a8
Toan <tpkhuong@gmail.com>
2021-01-10 07:41:15 -0500

    small changes

--- src/csmExports.js
+++ src/csmExports.js
@@ -1,4 +1,7 @@
 var ourSelectors = require("./selectors");
+// import { ourSelectors } from "./selectors";
+
+/* if we use CSM to import we have to use CSM exports. when we used ES import we got an error */
 
 var thisIsAnExample = {
   firstKey: "hello",
@@ -6,6 +9,7 @@ var thisIsAnExample = {
 };
 
 function printStuff(ourSelectorsInput) {
+  console.log(ourSelectors);
   console.log("This is from csmExports.js");
   ourSelectorsInput.divClickMe.addEventListener(
     "click",
